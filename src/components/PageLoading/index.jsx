f3971edff9ab85170c09cd8f95a12f9c37c46bdf
Kshitij <kshitij.sinha94@gmail.com>
2020-05-26 11:43:41 -0700

    some more basic changes

--- src/components/PageLoading/index.jsx
+++ src/components/PageLoading/index.jsx
@@ -1,4 +1,16 @@
 import { PageLoading } from '@ant-design/pro-layout'; // loading components from code split
 // https://umijs.org/plugin/umi-plugin-react.html#dynamicimport
+// import React from 'react';
+
+// const PageLoading = props => {
+//     return '';
+// }
+
 
 export default PageLoading;
+
+// import React from 'react';
+// declare const PageLoading: React.FC<{
+//     tip?: string;
+// }>;
+// export default PageLoading;
