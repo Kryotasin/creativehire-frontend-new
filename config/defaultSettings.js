75c6690980cc1ed6e05fc4ee36d31eb8725c069c
Kshitij <kshitij.sinha94@gmail.com>
2020-05-26 10:49:47 -0700

    intiial

---
+++ config/defaultSettings.js
@@ -0,0 +1,17 @@
+export default {
+  navTheme: 'dark',
+  // 拂晓蓝
+  primaryColor: '#011A27',
+  layout: 'sidemenu',
+  contentWidth: 'Fluid',
+  fixedHeader: false,
+  autoHideHeader: false,
+  fixSiderbar: false,
+  colorWeak: false,
+  menu: {
+    locale: true,
+  },
+  title: 'Creativehire',
+  pwa: false,
+  iconfontUrl: '',
+};
