77faf51e97c06e93ae5c1a5489e195b6527b70d2
Kshitij <kshitij.sinha94@gmail.com>
2021-01-22 16:26:59 -0800

    Updated tnc and privacy policy

--- src/pages/misc/privacy/index.jsx
+++ src/pages/misc/privacy/index.jsx
@@ -11,11 +11,11 @@ const Privacy = () => {
         <Row>
         <Col span={4}/>
         <Col span={16}>
-        <Title>Privacy Policy for CreativeHire</Title>
+        <Title level={3}>Privacy Policy for CreativeHire</Title>
 
           <p>At Creativehire, accessible from <a href="https://www.creativehire.co/">https://www.creativehire.co/</a>, one of our main priorities is the privacy of our visitors. This Privacy Policy document contains types of information that is collected and recorded by Creativehire and how we use it.</p>
 
-          <p>If you have additional questions or require more information about our Privacy Policy, do not hesitate to <Link to="/misc/contact/">contact us</Link>.</p>
+          <p>If you have additional questions or require more information about our Privacy Policy, do not hesitate to <a href="/misc/contact/">contact us</a>.</p>
 
           <p>This Privacy Policy applies only to our online activities and is valid for visitors to our website with regards to the information that they shared and/or collect in Creativehire. This policy is not applicable to any information collected offline or via channels other than this website.</p>
 
@@ -26,22 +26,22 @@ const Privacy = () => {
           <Title level={3}>Information we collect</Title>
 
           <p>The personal information that you are asked to provide, and the reasons why you are asked to provide it, will be made clear to you at the point we ask you to provide your personal information.</p>
-          <p>If you <Link to="/misc/contact/">contact us</Link> directly, we may receive additional information about you such as your name, email address, phone number, the contents of the message and/or attachments you may send us, and any other information you may choose to provide.</p>
+          <p>If you <a href="/misc/contact/">contact us</a> directly, we may receive additional information about you such as your name, email address, phone number, the contents of the message and/or attachments you may send us, and any other information you may choose to provide.</p>
           <p>When you register for an Account, we may ask for your contact information, including items such as name, location, company name, address, email address, and telephone number.</p>
 
           <Title level={3}>How we use your information</Title>
 
           <p>We use the information we collect in various ways, including to:</p>
 
-          <List>
-          <List.Item>Provide, operate, and maintain our website</List.Item>
-          <List.Item>Improve, personalize, and expand our website</List.Item>
-          <List.Item>Understand and analyze how you use our website</List.Item>
-          <List.Item>Develop new products, services, features, and functionality</List.Item>
-          <List.Item>Communicate with you, either directly or through one of our partners, including for customer service, to provide you with updates and other information relating to the website, and for marketing and promotional purposes</List.Item>
-          <List.Item>Send you emails</List.Item>
-          <List.Item>Find and prevent fraud</List.Item>
-          </List>
+          <ol>
+          <li>Provide, operate, and maintain our website</li>
+          <li>Improve, personalize, and expand our website</li>
+          <li>Understand and analyze how you use our website</li>
+          <li>Develop new products, services, features, and functionality</li>
+          <li>Communicate with you, either directly or through one of our partners, including for customer service, to provide you with updates and other information relating to the website, and for marketing and promotional purposes</li>
+          <li>Send you emails</li>
+          <li>Find and prevent fraud</li>
+          </ol>
 
           <Title level={3}>Log Files</Title>
 
@@ -75,7 +75,7 @@ const Privacy = () => {
           <p>Request that a business that collects a consumer's personal data disclose the categories and specific pieces of personal data that a business has collected about consumers.</p>
           <p>Request that a business delete any personal data about the consumer that a business has collected.</p>
           <p>Request that a business that sells a consumer's personal data, not sell the consumer's personal data.</p>
-          <p>If you make a request, we have one month to respond to you. If you would like to exercise any of these rights, please <Link to="/misc/contact/">contact us</Link>.</p>
+          <p>If you make a request, we have one month to respond to you. If you would like to exercise any of these rights, please <a href="/misc/contact/">contact us</a>.</p>
 
           <Title level={3}>GDPR Data Protection Rights</Title>
 
@@ -86,16 +86,16 @@ const Privacy = () => {
           <p>The right to restrict processing – You have the right to request that we restrict the processing of your personal data, under certain conditions.</p>
           <p>The right to object to processing – You have the right to object to our processing of your personal data, under certain conditions.</p>
           <p>The right to data portability – You have the right to request that we transfer the data that we have collected to another organization, or directly to you, under certain conditions.</p>
-          <p>If you make a request, we have one month to respond to you. If you would like to exercise any of these rights, please <Link to="/misc/contact/">contact us</Link>.</p>
+          <p>If you make a request, we have one month to respond to you. If you would like to exercise any of these rights, please <a href="/misc/contact/">contact us</a>.</p>
 
           <Title level={3}>Children's Information</Title>
 
           <p>Another part of our priority is adding protection for children while using the internet. We encourage parents and guardians to observe, participate in, and/or monitor and guide their online activity.</p>
 
-          <p>Creativehire does not knowingly collect any Personal Identifiable Information from children under the age of 13. If you think that your child provided this kind of information on our website, we strongly encourage you to <Link to="/misc/contact/">contact us</Link> immediately and we will do our best efforts to promptly remove such information from our records.</p>
+          <p>Creativehire does not knowingly collect any Personal Identifiable Information from children under the age of 13. If you think that your child provided this kind of information on our website, we strongly encourage you to <a href="/misc/contact/">contact us</a> immediately and we will do our best efforts to promptly remove such information from our records.</p>
 
 
-          <Title>Disclaimer for CreativeHire</Title>
+          <Title level={3}>Disclaimer for CreativeHire</Title>
 
           <p>If you require any more information or have any questions about our site's disclaimer, please feel free to contact us.</p>
 
--- src/pages/misc/terms/index.jsx
+++ src/pages/misc/terms/index.jsx
@@ -15,9 +15,9 @@ const Terms = () => {
       <Row>
       <Col span={4}/>
       <Col span={16}>
-      <Title>Terms and Conditions for CreativeHire</Title>
+      <Title level={2}>Terms and Conditions for CreativeHire</Title>
 
-        <Title level ={3}>Introduction</Title> 
+        <Title level={3}>Introduction</Title> 
         
         <p>These Website Standard Terms and Conditions written on this webpage shall manage your use of our website, Creativehire accessible at <a href="https://www.creativehire.co/">https://www.creativehire.co/</a>.</p>
 
@@ -25,68 +25,68 @@ const Terms = () => {
 
         <p>Minors or people below 18 years old are not allowed to use this Website.</p>
 
-        <Title level ={3}>Intellectual Property Rights</Title>
+        <Title level={3}>Intellectual Property Rights</Title>
 
         <p>Other than the content you own, under these Terms, CreativeHire and/or its licensors own all the intellectual property rights and materials contained in this Website.</p>
 
         <p>You are granted limited license only for purposes of viewing the material contained on this Website.</p>
 
-        <Title level ={3}>Restrictions</Title>
+        <Title level={3}>Restrictions</Title>
 
         <p>You are specifically restricted from all of the following:</p>
 
-        <List size="small">
-            <List.Item>Publishing any Website material in any other media;</List.Item>
-            <List.Item>Selling, sublicensing and/or otherwise commercializing any Website material;</List.Item>
-            <List.Item>Publicly performing and/or showing any Website material;</List.Item>
-            <List.Item>Using this Website in any way that is or may be damaging to this Website;</List.Item>
-            <List.Item>Using this Website in any way that impacts user access to this Website;</List.Item>
-            <List.Item>Using this Website contrary to applicable laws and regulations, or in any way may cause harm to the Website, or to any person or business entity;</List.Item>
-            <List.Item>Engaging in any data mining, data harvesting, data extracting or any other similar activity in relation to this Website;</List.Item>
-            <List.Item>Using this Website to engage in any advertising or marketing.</List.Item>
-        </List>
+        <ol>
+            <li>Publishing any Website material in any other media;</li>
+            <li>Selling, sublicensing and/or otherwise commercializing any Website material;</li>
+            <li>Publicly performing and/or showing any Website material;</li>
+            <li>Using this Website in any way that is or may be damaging to this Website;</li>
+            <li>Using this Website in any way that impacts user access to this Website;</li>
+            <li>Using this Website contrary to applicable laws and regulations, or in any way may cause harm to the Website, or to any person or business entity;</li>
+            <li>Engaging in any data mining, data harvesting, data extracting or any other similar activity in relation to this Website;</li>
+            <li>Using this Website to engage in any advertising or marketing.</li>
+        </ol>
 
         <p>Certain areas of this Website are restricted from being access by you and CreativeHire may further restrict access by you to any areas of this Website, at any time, in absolute discretion. Any user ID and password you may have for this Website are confidential and you must maintain confidentiality as well.</p>
 
-        <Title level ={3}>Your Content</Title>
+        <Title level={3}>Your Content</Title>
 
         <p>In these Website Standard Terms and Conditions, "Your Content" shall mean any audio, video text, images or other material you choose to display on this Website. By displaying Your Content, you grant CreativeHire a non-exclusive, worldwide irrevocable, sub licensable license to use, reproduce, adapt, publish, translate and distribute it in any and all media.</p>
 
         <p>Your Content must be your own and must not be invading any third-party’s rights. CreativeHire reserves the right to remove any of Your Content from this Website at any time without notice.</p>
 
-        <Title level ={3}>Your Privacy</Title>
+        <Title level={3}>Your Privacy</Title>
 
-        <p>Please read <Link to="privacy-policy">Privacy Policy</Link>.</p>
+        <p>Please read <a href="privacy-policy">Privacy Policy</a>.</p>
 
-        <Title level ={3}>No warranties</Title>
+        <Title level={3}>No warranties</Title>
 
         <p>This Website is provided "as is," with all faults, and CreativeHire express no representations or warranties, of any kind related to this Website or the materials contained on this Website. Also, nothing contained on this Website shall be interpreted as advising you.</p>
 
-        <Title level ={3}>Limitation of liability</Title>
+        <Title level={3}>Limitation of liability</Title>
 
         <p>In no event shall CreativeHire, nor any of its officers, directors and employees, shall be held liable for anything arising out of or in any way connected with your use of this Website whether such liability is under contract.  CreativeHire, including its officers, directors and employees shall not be held liable for any indirect, consequential or special liability arising out of or in any way related to your use of this Website.</p>
 
-        <Title level ={3}>Indemnification</Title>
+        <Title level={3}>Indemnification</Title>
 
         <p>You hereby indemnify to the fullest extent CreativeHire from and against any and/or all liabilities, costs, demands, causes of action, damages and expenses arising in any way related to your breach of any of the provisions of these Terms.</p>
 
-        <Title level ={3}>Severability</Title>
+        <Title level={3}>Severability</Title>
 
         <p>If any provision of these Terms is found to be invalid under any applicable law, such provisions shall be deleted without affecting the remaining provisions herein.</p>
 
-        <Title level ={3}>Variation of Terms</Title>
+        <Title level={3}>Variation of Terms</Title>
 
         <p>CreativeHire is permitted to revise these Terms at any time as it sees fit, and by using this Website you are expected to review these Terms on a regular basis.</p>
 
-        <Title level ={3}>Assignment</Title>
+        <Title level={3}>Assignment</Title>
 
         <p>The CreativeHire is allowed to assign, transfer, and subcontract its rights and/or obligations under these Terms without any notification. However, you are not allowed to assign, transfer, or subcontract any of your rights and/or obligations under these Terms.</p>
 
-        <Title level ={3}>Entire Agreement</Title>
+        <Title level={3}>Entire Agreement</Title>
             
         <p>These Terms constitute the entire agreement between CreativeHire and you in relation to your use of this Website, and supersede all prior agreements and understandings.</p>
 
-        <Title level ={3}>Governing Law & Jurisdiction</Title>
+        <Title level={3}>Governing Law & Jurisdiction</Title>
 
         <p>These Terms will be governed by and interpreted in accordance with the laws of the State of us, and you submit to the non-exclusive jurisdiction of the state and federal courts located in us for the resolution of any disputes.</p>
 
