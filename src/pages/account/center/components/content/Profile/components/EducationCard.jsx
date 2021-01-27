2133d8c4a71c4c649d91293645a895b43f85608a
Kshitij <kshitij.sinha94@gmail.com>
2021-01-26 23:02:32 -0800

    Update EducationCard.jsx

--- src/pages/account/center/components/content/Profile/components/EducationCard.jsx
+++ src/pages/account/center/components/content/Profile/components/EducationCard.jsx
@@ -99,7 +99,7 @@ const EducationCard = (props) => {
         out,
       )
       .then((res) => {
-        console.log(res.data)
+        // console.log(res.data)
         const updatedCandidatePart = Object.assign({}, res.data);
         const x = saveCandidate(updatedCandidatePart);
 
